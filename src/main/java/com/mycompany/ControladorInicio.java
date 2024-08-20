0d432eb92cf4bd168c4df7a0338a44f871719ab1
Omar Velazquez <ovelazquez.mx@gmail.com>
2024-08-19 20:55:36 -0600

    first commit

---
+++ src/main/java/com/mycompany/ControladorInicio.java
@@ -0,0 +1,17 @@
+package com.mycompany;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class ControladorInicio {
+    
+    @GetMapping("/")
+    public String inicio(){
+        log.info("Executing the Rest Controller");
+        return "Hello World from Spring Boot";
+    }
+    
+}
